59964e417f9e387d41b1022cd42f21bc0ad21237
Jihoon Kim <jihoon_kim@outlook.com>
2015-06-01 18:20:58 -0400

    Initial Commit

---
+++ MovingAverage.m
@@ -0,0 +1,20 @@
+load('Saliva_A');
+Saliva_A = david14otusmetadatasalivaA';
+
+Row_17 = Saliva_A(:,17);
+
+MA3 = conv(Row_17,[1/3,1/3,1/3]);
+MA5 = conv(Row_17,[1/5,1/5,1/5,1/5,1/5]);
+MA7 = conv(Row_17,[1/7,1/7,1/7,1/7,1/7,1/7,1/7]);
+
+figure
+plot(Row_17)
+hold on
+plot(MA3,'r','LineWidth',2)
+plot(MA5,'g','LineWidth',2)
+plot(MA7,'k','LineWidth',2)
+legend('Origianl','MA3','MA5','MA7')
+title('OTU 2222')
+xlabel('Days')
+hold off
+
