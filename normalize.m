0c51e4e05bcd2b007a904d6d90a2e3ca3c548802
Jihoon Kim <jihoon_kim@outlook.com>
2015-06-03 14:04:37 -0400

    FIX: divied by 0 case is replaced to 0 value to avoid NaN

--- normalize.m
+++ normalize.m
@@ -3,11 +3,15 @@ function output = normalize(input)
 
 [row,column] = size(input);
 
-output = zeros([row,1]);
+output = zeros(row,column);
 
 for i = 1 : row
     for j = 1: column
-        output(i,j) = (input(i,j)-min(input(i,:)))/(max(input(i,:))-min(input(i,:)));
+        if (max(input(i,:)) - min(input(i,:)) == 0)
+            output(i,j) = 0;
+        else
+            output(i,j) = (input(i,j)-min(input(i,:)))/(max(input(i,:))-min(input(i,:)));
+        end
     end
 end
 
