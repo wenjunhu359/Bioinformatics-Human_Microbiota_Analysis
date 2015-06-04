f764234f645181f80701869b9754cc9dc0b105a9
Jihoon Kim <jihoon_kim@outlook.com>
2015-06-03 22:12:28 -0400

    Displaying part is integrated

--- MovingAverage.m
+++ MovingAverage.m
@@ -12,20 +12,10 @@ for i = 1:msa_row
     end
 end
 
-diff = detect_diff_A(msa_avail);
-% 
-% MA3 = conv(Row_17,[1/3,1/3,1/3]);
-% MA5 = conv(Row_17,[1/5,1/5,1/5,1/5,1/5]);
-% MA7 = conv(Row_17,[1/7,1/7,1/7,1/7,1/7,1/7,1/7]);
-% 
-% figure
-% plot(Row_17)
-% hold on
-% plot(MA3,'r','LineWidth',2)
-% plot(MA5,'g','LineWidth',2)
-% plot(MA7,'k','LineWidth',2)
-% legend('Origianl','MA3','MA5','MA7')
-% title('OTU 2222')
-% xlabel('Days')
-% hold off
-% 
+diff = detect_diff_A(msa_avail, 0.18);
+diff_index = find(diff);
+
+ext_data = msa_avail(diff_index,:);
+ext_index = msa_avail_index(diff_index);
+
+display_ext_data(ext_data,ext_index)
